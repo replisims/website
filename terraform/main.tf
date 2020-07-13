b0c7adee885b8ce40e9f90d1db7333918635ee78
Peter Lohmann <peter.lohmann@dtn.com>
2020-07-13 08:32:40 +0200

    :bug: terraform: Less terraform plan diffs

--- terraform/main.tf
+++ terraform/main.tf
@@ -123,7 +123,8 @@ resource "aws_cloudfront_distribution" "website" {
   viewer_certificate {
     # TODO domain cert
     cloudfront_default_certificate = true
-    ssl_support_method       = "sni-only"
+    ssl_support_method             = "sni-only"
+    minimum_protocol_version       = "TLSv1"
   }
 }
 
