91c1d60e5665199894515a2c95a94fb96fcf420a
Peter Lohmann <peter.lohmann@dtn.com>
2020-07-13 08:38:07 +0200

    :wrench: Dummy to trigger terraform cloud

--- terraform/main.tf
+++ terraform/main.tf
@@ -121,7 +121,7 @@ resource "aws_cloudfront_distribution" "website" {
   }
 
   viewer_certificate {
-    # TODO domain cert
+    # TODO  domain cert
     cloudfront_default_certificate = true
     ssl_support_method             = "sni-only"
     minimum_protocol_version       = "TLSv1"
